1a93d81a7ad9bad1f19e8829e9b0a074e0924f46
Andrew Vo-Nguyen <48583281+andrewvo89@users.noreply.github.com>
2021-04-29 07:58:04 +1000

    Initial Commit

---
+++ places-autocomplete-swift-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  places-autocomplete-swift-uikit
+//
+//  Created by Andrew Vo-Nguyen on 29/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
